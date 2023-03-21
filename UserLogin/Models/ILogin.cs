bf93571cf6a684270c07fe18ef71f2ac0ba87b3b
ZeynepMirzooglu <100854123+ZeynepMirzooglu@users.noreply.github.com>
2023-03-21 11:25:58 +0300

    UserLogin
    UserLogin

---
+++ UserLogin/Models/ILogin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UserLogin.Models
+{
+    public interface ILogin
+    {
+        Task<IEnumerable<User>> GetUser();
+        Task<User> AuthenticateUser(string tcNo, string password);
+
+
+    }
+}
